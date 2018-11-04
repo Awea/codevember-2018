f2396dc86275e31521f6929f388de67527f3263d
Awea <aweaoftheworld@gmail.com>
2018-11-04 13:39:55 +0100

    :sparkles: 03 - Triangulation de Delaunay and 04 - K-moyennes

---
+++ src/03.js
@@ -0,0 +1,36 @@
+const canvasSketch = require('canvas-sketch')
+const random       = require('canvas-sketch-util/random')
+const triangulate  = require('delaunay-triangulate') 
+
+const settings = {};
+
+const colors  = [
+  "#F5C80B",
+  "#AC0025",
+  "#2D7B2F",
+  "#DE5504",
+  "#02569F",
+  "#3A2D5B"
+]
+
+// Start the sketch
+canvasSketch(() => {
+  return ({ context, width, height, playhead, time }) => {
+    let coordinates = [...Array(400)].map(() => random.insideCircle(height / 2)) 
+    let triangles   = triangulate(coordinates)
+
+    context.translate(width / 2, height / 2)
+
+    triangles.forEach((triangle) => {
+      let triangleCoordinates = triangle.map(i => coordinates[i])
+
+      context.fillStyle = colors[random.rangeFloor(0, colors.length)]
+      context.beginPath()
+      context.moveTo(triangleCoordinates[0][0], triangleCoordinates[0][1])
+      context.lineTo(triangleCoordinates[1][0], triangleCoordinates[1][1])
+      context.lineTo(triangleCoordinates[2][0], triangleCoordinates[2][1])
+      context.fill()
+      context.stroke()
+    })
+  };
+}, settings);
---
+++ src/04.js
@@ -0,0 +1,62 @@
+// Heavily inspired by: https://mattdesl.svbtle.com/pen-plotter-2
+
+const canvasSketch        = require('canvas-sketch')
+const random              = require('canvas-sketch-util/random')
+const clustering          = require('density-clustering')
+const convexHull          = require('convex-hull')
+const { renderPolylines } = require('canvas-sketch-util/penplot');
+
+const settings = {};
+
+const colors  = [
+  "#F5C80B",
+  "#AC0025",
+  "#2D7B2F",
+  "#DE5504",
+  "#02569F",
+  "#3A2D5B"
+]
+
+// Start the sketch
+canvasSketch(() => {
+  return ({ context, width, height, playhead, time }) => {
+    let coordinates  = [...Array(4000)].map(() => random.insideCircle(height / 2)) 
+    let clusterCount = 3
+    let scan         = new clustering.KMEANS()
+
+    context.translate(width / 2, height / 2)
+
+    while (coordinates.length >= clusterCount) {
+      // Create clusters of points sorted by density
+      const clusters = scan
+        .run(coordinates, clusterCount)
+        .filter(c => c.length >= 3)
+        .sort((a, b) => a.length - b.length);
+
+      // Ensure we resulted in some clusters
+      if (clusters.length === 0) return;
+
+      // Select the least dense cluster
+      const cluster = clusters[0];
+
+      const positions = cluster.map(i => coordinates[i]);
+
+      // Create a closed polyline from the hull
+      let path = convexHull(positions).map(c => positions[c[0]]);
+      path.push(path[0]);
+
+      // Add to total list of polylines
+      let lines = []
+      lines.push(path);
+
+      // Remove those coordinates from our data set
+      coordinates = coordinates.filter(p => !positions.includes(p));
+
+      context.fillStyle = colors[random.rangeFloor(0, colors.length)]
+      context.beginPath()
+      path.forEach((dot) => context.lineTo(dot[0], dot[1]))
+      context.fill()
+      context.stroke()
+    }
+  };
+}, settings);
